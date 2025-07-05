ea7e02e4765504933851f56b4650023529462c8e
khushmnsangha <anmolsangha02@gmail.com>
2025-07-05 21:08:46 +0530

    made code deploy ready

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const server = http.createServer(app);
 const io = require('socket.io')(server, { cors: { origin: '*' } });
@@ -15,6 +16,12 @@ app.use(express.json());
 
 app.use('/leaderboard', require('./routes/leaderboard'));
 require('./socketManager')(io);
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
